6bcfb8b14237b52c8433e5851057ce71518f8952
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-01 09:46:15 -0700

    moved data

---
+++ data/friends.js
@@ -0,0 +1,86 @@
+const friendsArray = [
+    {
+    "name":"Shakira",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj6qb2PqY_jAhVHxVQKHce0AAQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.billboard.com%2Farticles%2Fcolumns%2Flatin%2F8221070%2Fshakira-pays-20-million-euros-back-taxes-spanish-government&psig=AOvVaw1Gg0DGbmE9MQ3X8PjrVrAy&ust=1561919340600735",
+    "scores":[
+        5,
+        1,
+        2,
+        2,
+        1,
+        1,
+        2,
+        4,
+        3,
+        1
+      ]
+  },
+  {
+    "name":"Jennifer Lopez",
+    "photo":"https://cfda.imgix.net/2019/04/JLo-headshot-by-Gabriel-Goldman.jpg?w=384&dpr=2.625&q=65&auto=format&q=35&fit=clip",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+  },
+  {
+    "name":"Leonardo DiCaprio",
+    "photo":"http://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg",
+    "scores":[
+        1,
+        1,
+        2,
+        2,
+        3,
+        3,
+        4,
+        4,
+        5,
+        5
+      ]
+  },
+  {
+    "name":"Brad Pitt",
+    "photo":"https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_.jpg",
+    "scores":[
+        5,
+        5,
+        4,
+        4,
+        5,
+        5,
+        5,
+        5,
+        4,
+        4
+      ]
+  },
+  {
+    "name":"Beyonce",
+    "photo":"https://media.wmagazine.com/photos/5ab3b3cd8f35206e675d7d28/4:3/w_1536/beyonce-pledges-to-build-more-wells-in-burundi.jpg",
+    "scores":[
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
+      ]
+  }
+];
+
+
+
+module.exports = friendsArray;
