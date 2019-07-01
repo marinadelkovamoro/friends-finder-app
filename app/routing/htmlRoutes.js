1cc3ce0374aaf7232e5766ff18f737742a3eb107
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-01 08:31:56 -0700

    app.get(/) changed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     });
 
     // A default, catch-all route that leads to home.html which displays the home page.
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }
